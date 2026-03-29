a207cdda6431f392bb8ac25982cf5eaac36f798b
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2026-03-29 03:18:16 +0000

    fix: correctly disable eslint set-state-in-effect rule

--- lib/hooks/useMounted.ts
+++ lib/hooks/useMounted.ts
@@ -1,15 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 
 function useMounted() {
-    const [mounted, setMounted] = useState(false)
+  const [mounted, setMounted] = useState(false)
 
-    useEffect(() => {
-        setMounted(true)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-  return (
-    mounted
-  )
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true)
+  }, [])
+  return mounted
 }
 
 export default useMounted
